06f03aa8808dafbc9c202443118cc958966a6f28
basarat <basaratali@gmail.com>
2022-06-07 17:26:38 +1000

    simpler

--- src/api.tsx
+++ src/api.tsx
@@ -13,15 +13,3 @@ let payload: Payload = {
 api.get('/', (_req, res) => {
   res.send(payload);
 });
-
-api.post('/', (req, res) => {
-  const payloadNext: Payload = req.body;
-  if (!payloadNext.message) {
-    res.status(400).send({
-      error: 'Invalid request: No message property',
-    });
-    return;
-  }
-  payload = payloadNext;
-  res.send(payload);
-});
