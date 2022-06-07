56970085ba0386ead74361d93012dd11901416dc
basarat <basaratali@gmail.com>
2022-06-07 17:27:50 +1000

    simpler

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 
 const Home: React.FC<{}> = () => {
   const [data, setData] = useState(null);
+  
   const onLoadData = async () => {
     const resp = await fetch('/api');
     const data = await resp.json();
     setData(data);
   };
+
   return (<>
     <button onClick={onLoadData}>Load Data</button>
     {data && <pre>{JSON.stringify(data, null, 4)}</pre>}
